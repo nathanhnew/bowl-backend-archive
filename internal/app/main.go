1a0ee288e54ef601b150295290bb93e04d9247dc
nate <nathanhnew@gmail.com>
2019-10-09 20:09:42 -0400

    migrate commit to separate package

--- internal/app/main.go
+++ internal/app/main.go
@@ -2,45 +2,35 @@ package main
 
 import (
 	//"github.com/nathanhnew/bowl-backend/internal/app/controllers"
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
+	"github.com/nathanhnew/bowl-backend/internal/app/config"
 	"github.com/nathanhnew/bowl-backend/internal/app/db"
 	"github.com/nathanhnew/bowl-backend/internal/models"
 	//"github.com/gorilla/mux"
-	"io/ioutil"
+	//"io/ioutil"
 	//"log"
 	//"net/http"
 	"go.mongodb.org/mongo-driver/bson"
-	"os"
+	//"os"
 )
-
-func getConfig() (map[string]interface{}, error) {
-	cfgFile, err := os.Open("config/main.conf.json")
-	if err != nil {
-		return nil, err
-	}
-	defer cfgFile.Close()
-	byteCfg, _ := ioutil.ReadAll(cfgFile)
-
-	var cfg map[string]interface{}
-	json.Unmarshal([]byte(byteCfg), &cfg)
-
-	return cfg, nil
-}
 
 func main() {
 	//r := mux.NewRouter()
-	cfg, _ := getConfig()
-	fmt.Println(cfg)
+	cfg, err := config.GetConfig(config.DefaultConfigLocation)
+	if err != nil {
+		fmt.Println(err)
+	}
 	//port := int(cfg["port"].(float64))
-	mongoUri := cfg["mongoUri"].(string)
+	mongoUri := cfg.GetMongoUri()
 	ctx := db.GetContext(30)
 	client := db.Connect(mongoUri)
+	defer db.Disconnect(client)
 	var user models.User
 	collection := client.Database("application").Collection("User")
 	_ = collection.FindOne(ctx, bson.D{}).Decode(&user)
 	fmt.Printf("%+v\n", user)
-	fmt.Printf("Router initialized on port %d\n", int(cfg["port"].(float64)))
+	fmt.Printf("Router initialized on port %d\n", cfg.GetListenPort())
 	//r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
 	//	http.FileServer(http.Dir("../assets/"))))
 	//r.HandleFunc("/teams", controllers.GetAllTeams).Methods("GET")
