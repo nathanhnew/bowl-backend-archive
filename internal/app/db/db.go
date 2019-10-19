815cb5fd6637213070dd7f1d319c32d9971e0b83
nate <nathanhnew@gmail.com>
2019-10-19 18:57:06 -0400

    add defaults for db connections

--- internal/app/db/db.go
+++ internal/app/db/db.go
@@ -3,15 +3,26 @@ package db
 import (
 	"context"
 	"fmt"
+	"github.com/nathanhnew/bowl-backend/internal/app/config"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"time"
 )
 
+var cfg, _ = config.GetConfig(config.DefaultConfigLocation)
+var ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+var Client *mongo.Client = Connect(cfg.GetMongoUri())
+var database = cfg.Values["mongoDatabase"].(string)
+var userCollection = "User"
+var schoolCollection = "School"
+var leagueCollection = "League"
+var bowlCollection = "Bowl"
+var pickCollection = "PickList"
+
 func Connect(uri string) *mongo.Client {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, _ := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	ctx = getContext()
+	client, _ := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
 	err := client.Ping(ctx, readpref.Primary())
 	if err != nil {
 		fmt.Println(err)
@@ -19,12 +30,12 @@ func Connect(uri string) *mongo.Client {
 	return client
 }
 
-func GetContext(timeout time.Duration) context.Context {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	return ctx
+func Disconnect(client *mongo.Client) {
+	ctx = getContext()
+	client.Disconnect(ctx)
 }
 
-func Disconnect(client *mongo.Client) {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client.Disconnect(ctx)
+func getContext() context.Context {
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	return ctx
 }
