4aeff4ef35595c69dbbd14846dcad37d5bda3a90
nate <nathanhnew@gmail.com>
2019-10-19 18:56:24 -0400

    create update method for struct

--- internal/models/school.go
+++ internal/models/school.go
@@ -21,10 +21,34 @@ type schoolName struct {
 	LongName     string `bson:"longName"`
 	ShortName    string `bson:"shortName"`
 	Abbreviation string `bson:"abbreviation"`
+	Slug         string `bson:"slug"`
 }
 
 type conference struct {
 	StartDate time.Time  `bson:"startDate"`
 	EndDate   *time.Time `bson:"endDate"`
 	Name      string     `bson:"name"`
+}
+
+func (school *School) UpdateFromMap(payload map[string]interface{}) {
+	for k, v := range payload {
+		if k == "longName" {
+			school.Name.LongName = v.(string)
+		}
+		if k == "shortName" {
+			school.Name.ShortName = v.(string)
+		}
+		if k == "abbreviation" {
+			school.Name.Abbreviation = v.(string)
+		}
+		if k == "slug" {
+			school.Name.Slug = v.(string)
+		}
+		if k == "teamName" {
+			school.TeamName = v.(string)
+		}
+		if k == "logo" {
+
+		}
+	}
 }
