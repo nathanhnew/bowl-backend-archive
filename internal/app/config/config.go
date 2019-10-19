e5266ec4d01ffc211c725f1be9e7ca5d1ba597eb
nate <nathanhnew@gmail.com>
2019-10-19 18:55:26 -0400

    export config struct

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -10,11 +10,13 @@ import (
 var DefaultConfigLocation string = "config/main.conf.json"
 
 type Config struct {
-	values   map[string]interface{}
-	location string
+	Values   map[string]interface{}
+	Location string
 }
 
 func GetConfig(location string) (*Config, error) {
+	var values map[string]interface{}
+	var cfg Config
 	if location == "" {
 		location = DefaultConfigLocation
 	}
@@ -25,24 +27,22 @@ func GetConfig(location string) (*Config, error) {
 	defer cfgFile.Close()
 	byteCfg, _ := ioutil.ReadAll(cfgFile)
 
-	var values map[string]interface{}
 	json.Unmarshal([]byte(byteCfg), &values)
 
-	var cfg Config
-	cfg.values = values
-	cfg.location = location
+	cfg.Values = values
+	cfg.Location = location
 
 	return &cfg, nil
 }
 
 func (config *Config) GetListenPort() int {
-	return int(config.values["port"].(float64))
+	return int(config.Values["port"].(float64))
 }
 
 func (config *Config) GetMongoUri() string {
-	return config.values["mongoUri"].(string)
+	return config.Values["mongoUri"].(string)
 }
 
 func (config *Config) GetMongoTimeout() time.Duration {
-	return config.values["timeout"].(time.Duration)
+	return config.Values["timeout"].(time.Duration)
 }
