cfcf6fdcb1ab80f76e84d5d68557d58300b5f445
nate <nathanhnew@gmail.com>
2019-10-20 12:30:29 -0400

    migrate to slugs instead of object id

--- internal/models/pickList.go
+++ internal/models/pickList.go
@@ -6,22 +6,22 @@ import (
 )
 
 type PickList struct {
-	ID          primitive.ObjectID `bson:"_id"`
-	User        primitive.ObjectID `bson:"user"`
-	PickDate    time.Time          `bson:"pickDate"`
-	Season      string             `bson:"season"`
-	League      primitive.ObjectID `bson:"league"`
-	IsFinalized bool               `bson:"isFinalized"`
-	Picks       []pick             `bson:"picks"`
+	ID          primitive.ObjectID `json:"id" bson:"_id"`
+	User        string             `json:"user" bson:"user"`
+	PickDate    time.Time          `json:"pickDate" bson:"pickDate"`
+	Season      string             `json:"season" bson:"season"`
+	League      string             `json:"league" bson:"league"`
+	IsFinalized bool               `json:"isFinalized" bson:"isFinalized"`
+	Picks       []pick             `json:"picks" bson:"picks"`
 	CreateTime  time.Time          `bson:"createTime"`
 	UpdateTime  time.Time          `bson:"updateTime"`
 }
 
 type pick struct {
-	Bowl        primitive.ObjectID `bson:"bowl"`
-	Pick        primitive.ObjectID `bson:"pick"`
-	Confidence  int                `bson:"confidence"`
-	PointValue  float64            `bson:"pointValue"`
-	HasOccurred bool               `bson:"hasOccurred"`
-	IsCorrect   bool               `bson:"isCorrect"`
+	Bowl        string  `json:"bowl" bson:"bowl"`
+	Pick        string  `json:"pick" bson:"pick"`
+	Confidence  int     `json:"confidence" bson:"confidence"`
+	PointValue  float64 `json:"pointValue" bson:"pointValue"`
+	HasOccurred bool    `json:"hasOccurred" bson:"hasOccurred"`
+	IsCorrect   bool    `json:"isCorrect,omitempty" bson:"isCorrect,omitempty"`
 }
